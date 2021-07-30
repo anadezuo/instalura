9b8b130d10b636b3dc575f33d87ddad56c621458
Ana Dezuó <ana.dezuo.apd@gmail.com>
2021-07-30 09:56:18 -0300

    Instalura - Aula 2
    testando tags do Open Graph

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -24,6 +24,22 @@ export function HeadTab() {
       <link rel="manifest" href="/site.webmanifest" />
       <meta name="theme-color" content="#ffffff" />
 
+      <meta
+        name="twitter:image:src"
+        content="https://www.alura.com.br/assets/img/bootcamp/front-end-jamstack/og-image.1616501197.png"
+      />
+      <meta name="twitter:site" content="@anadezuo" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:title"
+        content="Instalura"
+      />
+      <meta
+        name="twitter:description"
+        content="Quem saber como está essa rede social? Vem conferir  essa construção durante o Bootcamp Front End Avançado da Alura!"
+      />
+
+
 
       <meta
         property="og:image"
@@ -33,7 +49,6 @@ export function HeadTab() {
         property="og:image:alt"
         content="Imersão Alura - 3º Edição. Criando o Alurakut, baseado no antigo Orkut. Usando ReactJs, NextJs e StyledComponents, em um semana de muito aprendizado. - anadezuo/alurakut: Imersão Alura - 3º Edição...."
       />
-
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="600" />
       <meta property="og:site_name" content="Bootcamp Front End Avançado" />
