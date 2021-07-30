594d8ccb1854234020c332c37d91df97da8a0765
Ana Dezuó <ana.dezuo.apd@gmail.com>
2021-07-30 09:44:05 -0300

    Instalura - Aula 2
    testando tags do Open Graph

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -24,17 +24,25 @@ export function HeadTab() {
       <link rel="manifest" href="/site.webmanifest" />
       <meta name="theme-color" content="#ffffff" />
 
-      <meta property="og:site_name" content="Bootcamp Front End Avançado" />
-      <meta property="og:url" content="http://vercel.com/anadezuo/instalura" />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content="Instalura" />
-      <meta
-        property="og:description"
-        content="Quem saber como está essa rede social? Vem conferir  essa construção durante o Bootcamp Front End Avançado da Alura!"
-      />
+
       <meta
         property="og:image"
         content="https://www.alura.com.br/assets/img/bootcamp/front-end-jamstack/og-image.1616501197.png"
+      />
+      <meta
+        property="og:image:alt"
+        content="Imersão Alura - 3º Edição. Criando o Alurakut, baseado no antigo Orkut. Usando ReactJs, NextJs e StyledComponents, em um semana de muito aprendizado. - anadezuo/alurakut: Imersão Alura - 3º Edição...."
+      />
+
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+      <meta property="og:site_name" content="Bootcamp Front End Avançado" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="Instalura" />
+      <meta property="og:url" content="http://vercel.com/anadezuo/instalura" />
+      <meta
+        property="og:description"
+        content="Quem saber como está essa rede social? Vem conferir  essa construção durante o Bootcamp Front End Avançado da Alura!"
       />
     </Head>
   )
